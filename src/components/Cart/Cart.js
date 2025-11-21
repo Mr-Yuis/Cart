cec4ff21ec0778e99b2304aa29cbd9439a2237d1
Mr-Yuis <cbjvno1@gmail.com>
2025-11-21 15:05:26 +0900

    fixError1

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,15 @@
 import { useSelector } from "react-redux";
-import Card from "../UI/Card";
 import classes from "./Cart.module.css";
 import CartItem from "./CartItem";
 
 const Cart = (props) => {
-  const cartItems = useSelector((state) => state.cart.cartItems);
+  // 여기만 이렇게 고치면 영원히 안 터집니다!
+  const cartItems = useSelector((state) => state.cart?.items || []);
+  // 또는
+  // const cartItems = useSelector((state) => state.cart.items) || [];
 
   return (
-    <Card className={classes.cart}>
+    <div className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
         {cartItems.map((item) => (
@@ -23,7 +25,7 @@ const Cart = (props) => {
           />
         ))}
       </ul>
-    </Card>
+    </div>
   );
 };
 
