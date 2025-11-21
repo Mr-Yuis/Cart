85ee0f30c59b8a5d9133daa3582d9ee949093c53
Mr-Yuis <cbjvno1@gmail.com>
2025-11-21 13:14:32 +0900

    firstCommit

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,10 +1,18 @@
-import classes from './CartButton.module.css';
+import { useDispatch, useSelector } from "react-redux";
+import { uiActions } from "../../store/ui-Slice";
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const carQuantity = useSelector((state) => state.cart.totalQuantity);
+
+  const toggleCartHandler = () => {
+    dispatch(uiActions.toggle());
+  };
+
   return (
-    <button className={classes.button}>
+    <button className="button" onClick={toggleCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className="badge">{carQuantity}</span>
     </button>
   );
 };
--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,24 +1,34 @@
-import classes from './CartItem.module.css';
+import { useDispatch } from "react-redux";
+import { cartActions } from "../../store/cart-slice";
 
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+  const dispatch = useDispatch();
+  const { id, title, quantity, total, price } = props.item;
+
+  const addItemHandler = () => {
+    dispatch(cartActions.addItemToCart(id, title, price));
+  };
+
+  const removeItemHandler = () => {
+    dispatch(cartActions.removeItemFromCart(id));
+  };
 
   return (
-    <li className={classes.item}>
+    <li className="item">
       <header>
         <h3>{title}</h3>
-        <div className={classes.price}>
-          ${total.toFixed(2)}{' '}
-          <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
+        <div className="price">
+          ${total.toFixed(2)}{" "}
+          <span className="itemprice">(${price.toFixed(2)}/item)</span>
         </div>
       </header>
-      <div className={classes.details}>
-        <div className={classes.quantity}>
+      <div className="details">
+        <div className="quantity">
           x <span>{quantity}</span>
         </div>
-        <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+        <div className="actions">
+          <button onClick={removeItemHandler}>-</button>
+          <button onClick={addItemHandler}>+</button>
         </div>
       </div>
     </li>
